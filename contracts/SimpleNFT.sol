5482d9a468cfc2638395e45e49c1033e63a32ed2
Marcx <haitianhero6@gmail.com>
2022-10-24 14:39:38 -0400

    Basic nft finished

--- contracts/SimpleNFT.sol
+++ contracts/SimpleNFT.sol
@@ -1,4 +1,22 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
-contract BasicNft {}
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract BasicNft is ERC721 {
+    uint256 private s_tokenCounter;
+
+    constructor() ERC721("Dogie", "Dog") {
+        s_tokenCounter = 0;
+    }
+
+    function mintNft() public returns (uint256) {
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter = s_tokenCounter + 1;
+        return s_tokenCounter;
+    }
+
+    function getTokenCounter() public view returns (uint256) {
+        return s_tokenCounter;
+    }
+}
